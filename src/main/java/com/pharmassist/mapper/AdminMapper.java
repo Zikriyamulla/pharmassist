395c517dea8777b558572db458ee3db09b5f2651
Zikriyamulla <mullamohammadzikriya@gmail.com>
2024-10-28 23:18:05 +0530

    fixed errors in AdminMapper

--- src/main/java/com/pharmassist/mapper/AdminMapper.java
+++ src/main/java/com/pharmassist/mapper/AdminMapper.java
@@ -1,9 +1,11 @@
 package com.pharmassist.mapper;
 
+import org.springframework.stereotype.Component;
+
 import com.pharmassist.entity.Admin;
 import com.pharmassist.requestdtos.AdminRequest;
 import com.pharmassist.responsedtos.AdminResponse;
-
+@Component
 public class AdminMapper {
 	public Admin mapToAdmin(AdminRequest request, Admin admin) {
 		admin.setEmail(request.getEmail());
