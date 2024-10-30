5a1547bdc9a118c5d975877e3eca3f9806330f7f
Zikriyamulla <mullamohammadzikriya@gmail.com>
2024-10-30 12:24:49 +0530

    Creating pharmacy Request dtos

---
+++ src/main/java/com/pharmassist/requestdtos/PharmacyRequest.java
@@ -0,0 +1,35 @@
+package com.pharmassist.requestdtos;
+
+public class PharmacyRequest {
+	private String pharmacyId;
+	private String name;
+	private String gstNo;
+	private String licenceNo;
+	
+	public String getPharmacyId() {
+		return pharmacyId;
+	}
+	public void setPharmacyId(String pharmacyId) {
+		this.pharmacyId = pharmacyId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGstNo() {
+		return gstNo;
+	}
+	public void setGstNo(String gstNo) {
+		this.gstNo = gstNo;
+	}
+	public String getLicenceNo() {
+		return licenceNo;
+	}
+	public void setLicenceNo(String licenceNo) {
+		this.licenceNo = licenceNo;
+	}
+	
+
+}
