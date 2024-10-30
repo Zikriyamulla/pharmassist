516303076caec0bedf7f71f57fcb21ef1e80302f
Zikriyamulla <mullamohammadzikriya@gmail.com>
2024-10-30 12:20:53 +0530

    Creating pharmacy entity

---
+++ src/main/java/com/pharmassist/entity/Pharmacy.java
@@ -0,0 +1,41 @@
+package com.pharmassist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pharmacy {
+	@Id
+	private String pharmacyId;
+	private String name;
+	private String gstNo;
+	private String licenceNo;
+	
+	public String getPharmacyId() {
+		return pharmacyId;
+	}
+	public void setPharmacyId(String pharmacyId) {
+		this.pharmacyId = pharmacyId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGstNo() {
+		return gstNo;
+	}
+	public void setGstNo(String gstNo) {
+		this.gstNo = gstNo;
+	}
+	public String getLicenceNo() {
+		return licenceNo;
+	}
+	public void setLicenceNo(String licenceNo) {
+		this.licenceNo = licenceNo;
+	}
+	
+	
+
+}
