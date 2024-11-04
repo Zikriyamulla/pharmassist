4a6783533ed4c4cb24d4c4bb17a090b435b54307
Zikriyamulla <mullamohammadzikriya@gmail.com>
2024-11-04 23:36:20 +0530

    adding validations for PatientRequest

--- src/main/java/com/pharmassist/requestdtos/PatientRequest.java
+++ src/main/java/com/pharmassist/requestdtos/PatientRequest.java
@@ -4,12 +4,36 @@ import java.time.LocalDate;
 
 import com.pharmassist.enums.Gender;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public class PatientRequest {
+	@NotNull(message="PatientName Cannot be Null")
+	@NotBlank(message="PatientName Cannot be blank")
+	@Pattern(regexp = "^[a-zA-Z]+$",message="The PatientName must only contain alphabets")
 	private String name;
+	
+	@NotBlank(message="Phone Number Cannot be blank")
+	@NotNull(message="Phone Number cannot  be null")
+	@Pattern(regexp = "^[6-9][0-9]{9}$"
+			, message="Invalid phone number")
 	private String phoneNumber;
+	
+	@NotBlank(message="Email Cannot be blank")
+	@NotNull(message="Email Cannot be null")
+	@Pattern(regexp = "^[a-zA-Z\\._%+-]+@gmail\\.com$"
+			, message="Invalid Gmail Id")
 	private String email;
+	
+	@NotBlank(message="Gender Cannot be blank")
+	@NotNull(message="Gender Cannot be null")
 	private Gender gender;
+	
+	@NotBlank(message="DateOfBirth Cannot be blank")
+	@NotNull(message="DateOfBirth Cannot be null")
 	private LocalDate dateOfBirth;
+	
 	public String getName() {
 		return name;
 	}
