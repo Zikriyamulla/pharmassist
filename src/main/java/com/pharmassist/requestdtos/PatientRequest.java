57918a4b13a57dbf18fc67568588d955b3b11d22
Zikriyamulla <mullamohammadzikriya@gmail.com>
2024-11-04 22:51:19 +0530

    Creating patient Request in Requestdtos

---
+++ src/main/java/com/pharmassist/requestdtos/PatientRequest.java
@@ -0,0 +1,44 @@
+package com.pharmassist.requestdtos;
+
+import java.time.LocalDate;
+
+import com.pharmassist.enums.Gender;
+
+public class PatientRequest {
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private Gender gender;
+	private LocalDate dateOfBirth;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Gender getGender() {
+		return gender;
+	}
+	public void setGender(Gender gender) {
+		this.gender = gender;
+	}
+	public LocalDate getDateOfBirth() {
+		return dateOfBirth;
+	}
+	public void setDateOfBirth(LocalDate dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+}
