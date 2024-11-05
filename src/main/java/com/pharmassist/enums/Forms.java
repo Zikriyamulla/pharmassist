33685908202574e75d7ae316f1c58d031eb92645
Zikriyamulla <mullamohammadzikriya@gmail.com>
2024-11-05 12:30:04 +0530

    Creating MedicineEntity

---
+++ src/main/java/com/pharmassist/enums/Forms.java
@@ -0,0 +1,16 @@
+package com.pharmassist.enums;
+
+public enum Forms {
+	   CAPSULE,
+	    SYRUP,
+	    INJECTION,
+	    OINTMENT,
+	    CREAM,
+	    GEL,
+	    DROPS,
+	    INHALER,
+	    PATCH,
+	    POWDER,
+	    SUPPOSITORY;
+
+}
