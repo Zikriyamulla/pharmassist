fac3448049c1db71f1eb27264fafa3ad43a5dcf5
Zikriyamulla <mullamohammadzikriya@gmail.com>
2024-11-06 12:01:31 +0530

    adding Medicines in MedicineController and MedicineService from excel sheet

--- src/main/java/com/pharmassist/controller/MedicineController.java
+++ src/main/java/com/pharmassist/controller/MedicineController.java
@@ -1,9 +1,16 @@
 package com.pharmassist.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.pharmassist.service.MedicineService;
 import com.pharmassist.util.AppResponseBuilder;
+import com.pharmassist.util.SimpleResponseStructure;
 
 @RestController
 public class MedicineController {
@@ -14,7 +21,12 @@ public class MedicineController {
 		this.medicineService = medicineService;
 		this.responseBuilder = responseBuilder;
 	}
-	
+	@PostMapping("/pharmacy/{pharmacyId}/medicines")
+	public ResponseEntity<SimpleResponseStructure> uploadMedicines(@RequestParam MultipartFile file,@PathVariable String pharmacyId){
+		String medicines =medicineService.uploadMedicines(file,pharmacyId);
+		return responseBuilder.success(HttpStatus.CREATED,medicines);
+		
+	}
 	
 
 }
--- src/main/java/com/pharmassist/service/MedicineService.java
+++ src/main/java/com/pharmassist/service/MedicineService.java
@@ -1,14 +1,80 @@
 package com.pharmassist.service;
 
-import com.pharmassist.repository.MedicineRepository;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.pharmassist.entity.Medicine;
+import com.pharmassist.entity.Pharmacy;
+import com.pharmassist.enums.Forms;
+import com.pharmassist.exception.PharmacyNotFoundByIdException;
+import com.pharmassist.repository.MedicineRepository;
+import com.pharmassist.repository.PharmacyRepository;
+@Service
 public class MedicineService {
 	private final MedicineRepository medicineRepository ;
+	private final PharmacyRepository pharmacyRepository;
 
-	public MedicineService(MedicineRepository medicineRepository) {
+	public MedicineService(MedicineRepository medicineRepository, PharmacyRepository pharmacyRepository) {
 		super();
 		this.medicineRepository = medicineRepository;
+		this.pharmacyRepository = pharmacyRepository;
 	}
 	
+	public String uploadMedicines(MultipartFile file,String pharmacyId) {
+		List<Medicine> medicines = new ArrayList<>();
 
+		Pharmacy pharmacy = pharmacyRepository.findById(pharmacyId)
+				.orElseThrow(()->new PharmacyNotFoundByIdException("Failed to upload due to no Pharmacy found by Id "+pharmacyId));
+
+		try (XSSFWorkbook workBook= new XSSFWorkbook(file.getInputStream())){
+
+			for(Sheet sheet : workBook) {
+				for(Row row : sheet) {
+					if(row.getRowNum() != 0) {
+						Medicine medicine = new Medicine();
+						medicine.setName(row.getCell(0).getStringCellValue());
+						medicine.setCategory(row.getCell(1).getStringCellValue());
+						medicine.setDossageInMg((int) row.getCell(2).getNumericCellValue());
+
+
+
+						String formValue = row.getCell(3).getStringCellValue();
+						medicine.setForms(Forms.valueOf(formValue.toUpperCase()));
+
+						medicine.setIngredients(row.getCell(4).getStringCellValue());
+
+						medicine.setManufacture(row.getCell(5).getStringCellValue());
+						medicine.setStockQuantity(10);
+						medicine.setPrice(row.getCell(6).getNumericCellValue());
+
+						DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+						medicine.setExpireDate(LocalDate.parse(row.getCell(7).getStringCellValue(), formatter));
+
+						
+
+						medicine.setPharmacy(pharmacy);
+
+						medicines.add(medicine);
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		medicineRepository.saveAll(medicines);
+		return "Uploaded " + medicines.size() + " medicines successfully!";
+
+	}
+	
 }
